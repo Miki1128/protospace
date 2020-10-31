60d75651b5f264a7490e222af59cfaf1523b8d48
Miki1128 <urai.miki.1128@gmail.co.jp>
2020-11-01 01:48:35 +0900

    ProtoSpace挙動確認2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:imdex, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
